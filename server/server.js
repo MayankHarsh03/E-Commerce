687a5b1d5bf4563a7697c703b7d4f6ddf878c4fe
Mayank Harsh <mayankharsh@Mayanks-MacBook-Air.local>
2025-01-25 20:07:18 +0530

    changes in client side for connecting server to client

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ const PORT = process.env.PORT || 5001; // very important it will define server
 
 app.use(
     cors({
-        origin : "https://e-commerce-ruddy-five.vercel.app/auth/login",
+        origin : ["https://e-commerce-ruddy-five.vercel.app/auth/login"],
         method : ["GET", "POST", "DELETE", "PUT"],
         allowedHeaders : [
             "Content-Type",
